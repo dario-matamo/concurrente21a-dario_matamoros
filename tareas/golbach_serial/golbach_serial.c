524c9bed304744d14e872b8a1463e448a3a79f94
Dario Matamoros Vega <dario.matamoros@ucr.ac.cr>
2021-04-14 12:49:33 -0600

    Se anade el metodo de golbach para las conjeturas fuertes

--- tareas/golbach_serial/golbach_serial.c
+++ tareas/golbach_serial/golbach_serial.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 #include <math.h>
 int isPrime(int number);
-
+/*
+ * This method calculates the serial golbach sums in 
+ * even numbers
+ */
+void golbach(int number);
 int main(){
-    for(int i=0; i<100;i++){
-		if(isPrime(i)==0){
-			printf("%d is not prime\n",i);
-		}else{
-			printf("%d is prime\n",i);
+	int n = 8;
+	golbach(n);
+    return 0;
+}
+void golbach(int n){
+	int typeGolbach = n%2;
+	if(typeGolbach == 0){
+		//This cicle is for finding the sums of prime numbers
+		//Is divided by 2 because beyond that it will repeat the sums in
+		//the opposite order
+		for(int i=0;i<=n;i++){
+			if(isPrime(i)==1 && isPrime(n-i)==1){
+				printf("%d = %d + %d\n",n,i,n-i);
+			}
 		}
 	}
-    return 0;
 }
 int isPrime(int number){
 	int flag = 0;
@@ -24,3 +36,4 @@ int isPrime(int number){
 	}
 	return 1;
 }
+
