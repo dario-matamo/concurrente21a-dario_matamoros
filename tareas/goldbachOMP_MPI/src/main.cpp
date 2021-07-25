6c0cb73c55bdf5909c58eea964703c99d7075dc1
dario matamoros vega <dario.matamoros@ucr.ac.cr>
2021-07-25 10:11:32 -0600

    Inicio tarea OMP

---
+++ tareas/goldbachOMP_MPI/src/main.cpp
@@ -0,0 +1,10 @@
+// Copyright 2021 Donaldo Salas-Loria, Dario Matamoros, Manfred Carvajal
+#include <iostream>
+#include <omp.h>
+using namespace std;
+
+
+
+int main(int argc, char* argv[]) {
+  return 0;
+}
